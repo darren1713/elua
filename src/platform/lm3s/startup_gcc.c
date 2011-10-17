b847fad78bab78350b53231d809bf5424eefac30
James Snyder <jbsnyder@fanplastic.org>
2011-10-17 18:38:59 -0500

    add hard fault handler with register dump

--- src/platform/lm3s/startup_gcc.c
+++ src/platform/lm3s/startup_gcc.c
@@ -44,6 +44,7 @@ extern void UARTIntHandler();
 extern void CANIntHandler();
 
 #include "hw_memmap.h"
+#include <stdio.h>
 #include "platform_conf.h"
 
 extern void uart0_handler();
@@ -247,17 +248,52 @@ NmiSR(void)
 static void
 FaultISR(void)
 {
-    //
-    // Enter an infinite loop.
-    //
-   UARTCharPut( UART0_BASE, '#' );
-   UARTCharPut( UART0_BASE, '#' );
-   UARTCharPut( UART0_BASE, '#' );
-   UARTCharPut( UART0_BASE, '#' );
-   UARTCharPut( UART0_BASE, '#' );
-   while(1)
-   {
-   }
+  asm(
+  "tst lr, #4\n\t"
+  "ite eq\n\t"
+  "mrseq r0, msp\n\t"
+  "mrsne r0, psp\n\t"
+  "b hard_fault_handler_c"
+  );
+}
+
+void hard_fault_handler_c(unsigned int * hardfault_args)
+{
+  unsigned int stacked_r0;
+  unsigned int stacked_r1;
+  unsigned int stacked_r2;
+  unsigned int stacked_r3;
+  unsigned int stacked_r12;
+  unsigned int stacked_lr;
+  unsigned int stacked_pc;
+  unsigned int stacked_psr;
+
+  stacked_r0 = ((unsigned long) hardfault_args[0]);
+  stacked_r1 = ((unsigned long) hardfault_args[1]);
+  stacked_r2 = ((unsigned long) hardfault_args[2]);
+  stacked_r3 = ((unsigned long) hardfault_args[3]);
+
+  stacked_r12 = ((unsigned long) hardfault_args[4]);
+  stacked_lr = ((unsigned long) hardfault_args[5]);
+  stacked_pc = ((unsigned long) hardfault_args[6]);
+  stacked_psr = ((unsigned long) hardfault_args[7]);
+
+  printf ("[Hard fault handler]\n");
+  printf ("R0 = %x\n", stacked_r0);
+  printf ("R1 = %x\n", stacked_r1);
+  printf ("R2 = %x\n", stacked_r2);
+  printf ("R3 = %x\n", stacked_r3);
+  printf ("R12 = %x\n", stacked_r12);
+  printf ("LR = %x\n", stacked_lr);
+  printf ("PC = %x\n", stacked_pc);
+  printf ("PSR = %x\n", stacked_psr);
+  printf ("BFAR = %x\n", (*((volatile unsigned  *)(0xE000ED38))));
+  printf ("CFSR = %x\n", (*((volatile unsigned  *)(0xE000ED28))));
+  printf ("HFSR = %x\n", (*((volatile unsigned  *)(0xE000ED2C))));
+  printf ("DFSR = %x\n", (*((volatile unsigned  *)(0xE000ED30))));
+  printf ("AFSR = %x\n", (*((volatile unsigned  *)(0xE000ED3C))));
+
+  while (1) { ;; }
 }
 
 //*****************************************************************************
